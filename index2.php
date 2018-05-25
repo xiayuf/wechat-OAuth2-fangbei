4bea649a1e537bb48b3afdad27874355ebb391d2
Xia YuFeng <tiandaxyf@gmail.com>
2018-05-25 16:32:43 +0800

    Update index2.php

--- index2.php
+++ index2.php
@@ -6,7 +6,7 @@ $weixin = new class_weixin();
 if (!isset($_GET["code"])){
 	$redirect_url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 	$jumpurl = $weixin->oauth2_authorize($redirect_url, "snsapi_userinfo", "123");
-	Header("Location: $jumpurl");
+	Header("Location: $jumpurl");exit(); // 一定要header退出否则会回调失败！！！
 }else{
 	$access_token_oauth2 = $weixin->oauth2_access_token($_GET["code"]);
 	var_dump($access_token_oauth2);
